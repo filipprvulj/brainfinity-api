4fc042ff3cd463b0e83bd9c5b6331cc112ec2282
F <filip.prvulj@gmail.com>
2021-01-16 18:16:34 +0100

    Add Base repository

---
+++ Brainfinity.API/Brainfinity.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using Brainfinity.Data.Entities;
+using Brainfinity.Domain.Dtos;
+using Brainfinity.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainfinity.Data.Repositories
+{
+    public class BaseRepository<TEntity, TDto> : IBaseRepository<TDto>
+        where TEntity : BaseEntity
+        where TDto : BaseDto
+    {
+        public Task<int> EditEntity(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TDto> GetEntityById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TDto>> GetEntityPaginated(int page, int pageItemCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> InsertEntity(TDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> RemoveEntity(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Brainfinity.API/Brainfinity.Domain/RepositoryInterfaces/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Brainfinity.Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainfinity.Domain.RepositoryInterfaces
+{
+    public interface IBaseRepository<TDto>
+        where TDto : BaseDto
+    {
+        public Task<Guid> InsertEntity(TDto dto);
+
+        public Task<TDto> GetEntityById(Guid id);
+
+        public Task<List<TDto>> GetEntityPaginated(int page, int pageItemCount);
+
+        public Task<int> RemoveEntity(Guid id);
+
+        public Task<int> EditEntity(Guid id);
+    }
+}
