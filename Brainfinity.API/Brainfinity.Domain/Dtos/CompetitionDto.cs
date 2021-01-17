bc89da44e0668bb51c554f5dec0eab248c7830bd
F <filip.prvulj@gmail.com>
2021-01-17 15:29:03 +0100

    Add competition dto properties

--- Brainfinity.API/Brainfinity.Domain/Dtos/CompetitionDto.cs
+++ Brainfinity.API/Brainfinity.Domain/Dtos/CompetitionDto.cs
@@ -8,5 +8,10 @@ namespace Brainfinity.Domain.Dtos
 {
     public class CompetitionDto : BaseDto
     {
+        public string CompetitionName { get; set; }
+
+        public byte[] CompetitionPicture { get; set; }
+
+        public Guid StatusId { get; set; }
     }
 }
