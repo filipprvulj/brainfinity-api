99e99ec28201f448e1043aae3cf883456609dac7
F <filip.prvulj@gmail.com>
2021-01-17 13:53:01 +0100

    Add custom exceptions

--- Brainfinity.API/Brainfinity.Service/Services/BaseService.cs
+++ Brainfinity.API/Brainfinity.Service/Services/BaseService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Brainfinity.Data.Repositories;
+using Brainfinity.Domain.Dtos;
+using Brainfinity.Domain.RepositoryInterfaces;
+using Brainfinity.Domain.ServiceInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,40 @@ using System.Threading.Tasks;
 
 namespace Brainfinity.Service.Services
 {
-    class BaseService
+    public abstract class BaseService<TDto, TRepository> : IBaseService<TDto>
+        where TDto : BaseDto
+        where TRepository : IBaseRepository<TDto>
     {
+        protected readonly TRepository repository;
+
+        protected BaseService(TRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public Task<List<TDto>> GetEntitiesPaginated(int page, int pageItemCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TDto> GetEntityById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> InsertEntity(TDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> RemoveEntity(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateEntity(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
