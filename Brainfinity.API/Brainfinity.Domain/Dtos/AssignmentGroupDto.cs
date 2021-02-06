7339d106ba5a1c810210c4a56d65d8f9e82a2bba
Dejan Vukovic <dejan.vukovic101@gmail.com>
2021-02-06 18:09:29 +0100

    Adding AssignmentGroup Repository

---
+++ Brainfinity.API/Brainfinity.Domain/Dtos/AssignmentGroupDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainfinity.Domain.Dtos
+{
+    public class AssignmentGroupDto : BaseDto
+    {
+
+        public Guid CompetitionId { get; set; }
+
+        public Guid GradeId { get; set; }
+    }
+}
