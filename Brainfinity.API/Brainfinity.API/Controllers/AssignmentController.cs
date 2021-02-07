20dfeb2a83266b4ccd8a2e8e5c2594edf15aa842
Dejan Vukovic <dejan.vukovic101@gmail.com>
2021-02-07 18:14:13 +0100

    Adding Assignment Controller

---
+++ Brainfinity.API/Brainfinity.API/Controllers/AssignmentController.cs
@@ -0,0 +1,64 @@
+﻿using Brainfinity.Domain.Dtos;
+using Brainfinity.Domain.ServiceInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brainfinity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentController : ControllerBase
+    {
+        private readonly IAssignmentService service;
+
+        public AssignmentController(IAssignmentService service) 
+        {
+            this.service = service;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAssignment(Guid id)
+        {
+            var assignment = await service.GetEntityById(id);
+
+            return Ok(assignment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertAssignment(AssignmentDto assignment)
+        {
+            var insert = await service.InsertEntity(assignment);
+
+            return Ok(insert);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAssignments(int page, int pageItemCount)
+        {
+            var assignment = await service.GetEntitiesPaginated(page, pageItemCount);
+
+            return Ok(assignment);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveAssignment(Guid id)
+        {
+            await service.RemoveEntity(id);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAssignment(AssignmentDto assignment, Guid id)
+        {
+            await service.UpdateEntity(assignment, id);
+
+            return Ok();
+        }
+    }
+}
